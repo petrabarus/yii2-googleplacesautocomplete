9de12dc9281e1c8c7bd9a34116b67af7305e6d55
Petra Barus <petra.barus@gmail.com>
2015-01-16 13:09:08 +0700

    First commit.

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # Yii2-GooglePlacesAutoComplete
 Google Places Auto Complete widget for Yii2
+
+
+##Installation
+
+Add below to your `composer.json` file
+
+```
+    "repositories": [
+        {
+            "type": "vcs",
+            "url": "https://github.com/petrabarus/yii2-googleplacesautocomplete"
+        }
+    ],
+```
+
+##Usage
+
+Using widget and model.
+
+```
+use PetraBarus\Yii2\GooglePlacesAutoComplete\GooglePlacesAutoComplete;
+
+echo GooglePlacesAutoComplete::widget([
+    'model' => $model,
+    'attribute' => 'location'
+]);
+```
+
+Using widget for custom field name and value.
+
+```
+use PetraBarus\Yii2\GooglePlacesAutoComplete\GooglePlacesAutoComplete;
+echo GooglePlacesAutoComplete::widget([
+    'name' => 'place'
+    'value' => 'Jakarta'
+]);
+
+```
+
+Using active form.
+
+```
+use yii\bootstrap\ActiveForm;
+use PetraBarus\Yii2\GooglePlacesAutoComplete\GooglePlacesAutoComplete;
+
+echo $form = ActiveForm::begin();
+echo $form->field($model, 'location')->widget(GooglePlacesAutoComplete::className());
+```
---
+++ src/GooglePlacesAutoComplete.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace PetraBarus\Yii2\GooglePlacesAutoComplete;
+
+use yii\widgets\InputWidget;
+use yii\helpers\Html;
+
+
+class GooglePlacesAutoComplete extends InputWidget {
+
+    const API_URL = 'http://maps.googleapis.com/maps/api/js?';
+
+    public $libraries = 'places';
+
+    public $sensor = true;
+
+    public $autocompleteOptions = [];
+
+    /**
+     * Renders the widget.
+     */
+    public function run(){
+        $this->registerClientScript();
+        if ($this->hasModel()) {
+            echo Html::activeTextInput($this->model, $this->attribute, $this->options);
+        } else {
+            echo Html::textInput($this->name, $this->value, $this->options);
+        }
+    }
+
+    /**
+     * Registers the needed JavaScript.
+     */
+    public function registerClientScript(){
+        $elementId = $this->options['id'];
+        $scriptOptions = json_encode($this->autocompleteOptions);
+        $view = $this->getView();
+        $view->registerJsFile(self::API_URL . http_build_query([
+            'libraries' => $this->libraries,
+            'sensor' => $this->sensor ? 'true' : 'false'
+        ]));
+        $view->registerJs(<<<JS
+(function(){
+    var input = document.getElementById('{$elementId}');
+    var options = {$scriptOptions};
+
+    new google.maps.places.Autocomplete(input, options);
+})();
+JS
+        , \yii\web\View::POS_READY);
+    }
+}
